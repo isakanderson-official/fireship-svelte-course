0c43581c88fe950444c381f144bf44f33e5326ae
Isak Anderson <isak@isakanderson.com>
2024-07-22 14:12:20 -0500

    available username feature

---
+++ src/lib/components/AuthCheck.svelte
@@ -0,0 +1,10 @@
+<script>
+    import {user} from '$lib/firebase';
+</script>
+
+{#if $user}
+  <slot/>
+{:else}
+  <p class="text-center text-danger">You must be logged in to view this page</p>
+  <a class="btn btn-primary" href='/login'>Sign in</a>
+{/if}
---
+++ src/routes/yeet/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+  import AuthCheck from "$lib/components/AuthCheck.svelte";
+
+</script>
+
+<AuthCheck>
+    <h2>Yeet</h2>
+    <p>The word "yeet" originated from African American Vernacular English (AAVE) and gained popularity in mainstream culture through social media. It is often used as an exclamation or a verb to express excitement, energy, or forceful movement. The term has evolved to have various meanings and can be used in different contexts. In slang, "yeet" can also mean to discard or throw something with force. Its usage has become widespread and is now commonly used in memes, videos, and everyday conversations.</p>
+</AuthCheck>
